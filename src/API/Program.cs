15df813d0b649fe7b6987a5365135d22e1ac06a9
Juan Araya <juan-jose95@hotmail.com>
2026-01-14 10:54:42 +0000

    initial  FE setup

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -11,9 +11,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")); 
 });
 
+builder.Services.AddCors();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt =>
+{
+   opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000");
+});
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
